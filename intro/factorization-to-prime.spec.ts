4619a27dbf01b9fde89104a055ead8ff52212e7a
Dusan Trtica <x-dtrtica@zoox.com>
2026-01-23 21:20:04 +0100

    add missing number and num of divisible elements in interval assignments

--- intro/factorization-to-prime.spec.ts
+++ intro/factorization-to-prime.spec.ts
@@ -1,10 +1,11 @@
 import { describe, expect, it } from "vitest";
 
+
 const factorizeToPrimes = (n: number): Array<number> => {
     const constituents: Array<number> = []
 
     let i = 2;
-    while(n > 1) {
+    while(n > 1) {        
         while(n % i == 0) {
             constituents.push(i);
             n = Math.floor(n / i); 
@@ -21,6 +22,6 @@ describe('Factorization', () => {
     })
 
     it('disolves large prime number efficiently', () => {
-        expect(factorizeToPrimes(1000000007)).toEqual([2,2,3,3,5,5])
+        expect(factorizeToPrimes(1000000007)).toEqual([1000000007])
     })
 })
