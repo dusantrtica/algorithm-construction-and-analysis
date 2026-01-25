a1390f6e708d172010d20d4edb2079bdc135bb76
Dusan Trtica <dusan.trtica@hotmail.com>
2026-01-25 20:15:51 +0100

    add faster version for number or additive decomposition

--- complexity/descomposeToSumConsecutiveNumbers.spec.ts
+++ complexity/descomposeToSumConsecutiveNumbers.spec.ts
@@ -6,14 +6,14 @@ const decomposeToSumOfConsecutiveNumbers = (n: number): number => {
     let j = 1;
     let numOfCombination = 0;
 
-    while(i < n) {
-        if(sum == n) {
+    while (i < n) {
+        if (sum == n) {
             numOfCombination += 1;
             sum += i;
             i++;
-        } else if(sum < n) {
-            sum += i;       
-            i++;     
+        } else if (sum < n) {
+            sum += i;
+            i++;
         } else {
             sum -= j;
             j += 1;
@@ -22,8 +22,27 @@ const decomposeToSumOfConsecutiveNumbers = (n: number): number => {
     return numOfCombination;
 }
 
+const decomposeToSumOfConsecutiveNumbersByNumberOfOperands = (n: number): number => {
+    let numOfCombination = 0;
+    for (let m = 2; m + m * (m - 1) / 2 <= n; m++) {
+        if ((n - m * (m - 1) / 2) % m === 0) {
+            console.log('combination for ', m);
+            numOfCombination++;
+        }
+    }
+    
+    return numOfCombination;
+}
+
 describe('decomposeToSumOfConsecutiveNumbers', () => {
     it('returns 3 for number 15', () => {
+        // 1+2+3+4+5 = 15
+        // 4+5+6 = 15
+        // 7+8 = 15
         expect(decomposeToSumOfConsecutiveNumbers(15)).toBe(3);
+    })
+
+    it('returns 3 for 15, faster version', () => {
+        expect(decomposeToSumOfConsecutiveNumbersByNumberOfOperands(15)).toBe(3);
     })
 });
